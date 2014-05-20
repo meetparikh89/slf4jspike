cd51da8b949180673fc5c73b5c1b72779faf5ea5
meet <meet@meetlappie>
2014-05-20 13:16:05 +0530

    SLF4j Spike with Logback.

---
+++ src/main/java/my/demo/slf4j/MyClass.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.demo.slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author meet
+ */
+public class MyClass {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyClass.class);
+
+    public void bar() {
+
+        String name = "lordofthejars";
+
+        logger.info("Hello from Bar. - {}.",name);
+        logger.error("Fake error ;)");
+
+        logger.debug("In bar my name is {}.", name);
+
+    }
+    
+    public static void main(String[] args) {
+        new MyClass().bar();
+    }
+
+}
